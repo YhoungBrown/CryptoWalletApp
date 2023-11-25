4bd84927f93979a1943ab89bd0d28526adf2bcf3
Omotola <omotolaodumosu04@gmail.com>
2023-11-25 11:36:54 +0100

    graph works fine without error

--- components/Chart.js
+++ components/Chart.js
@@ -1,13 +1,56 @@
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions } from 'react-native'
 import React from 'react'
 //import { ChartDot, ChartPath, ChartPathProvider, ChartXLabel,ChartYLabel, monotoneCubicInterpolation } from '@rainbow-me/animated-charts';
+import{LineChart} from "react-native-chart-kit";
 
 import { SIZES, COLORS } from '../constants';
 
 const Chart = ({constainerStyle, chartPrices}) => {
   return (
     <View>
-      <Text style={{color: COLORS.white, alignItems: 'center', justifyContent: 'center'}}>Chart</Text>
+       <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "1",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
     </View>
   )
 }
